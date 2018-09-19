f7d37193b0aa455bd2af14463cb6549adcc72bf6
homer.yoon <homer.yoon@kakaocorp.com>
2018-09-20 01:30:19 +0900

    data helper

--- consts.py
+++ consts.py
@@ -5,7 +5,21 @@ Constants
 TRAIN_DATA_REPO = './input/ratings_train.txt'
 TEST_DATA_REPO = './input/ratings_test.txt'
 
+# Original data
 MORPHS_TRAIN_DOC_REPO = './input/morphs_ratings_train.tsv'
 NOUNS_TRAIN_DOC_REPO = './input/nouns_ratings_train.tsv'
 MORPHS_TEST_DOC_REPO = './input/morphs_ratings_test.tsv'
 NOUNS_TEST_DOC_REPO = './input/nouns_ratings_test.tsv'
+
+# Because of performance issue, parse and save.
+MORPHS_TRAIN_DOCUMENTS = './input/morphs_train_documents.txt'
+MORPHS_TRAIN_LABELS = './input/morphs_train_labels.txt'
+MORPHS_TEST_DOCUMENTS = './input/morphs_test_documents.txt'
+MORPHS_TEST_LABELS = './input/morphs_test_labels.txt'
+NOUNS_TRAIN_DOCUMENTS = './input/nouns_train_documents.txt'
+NOUNS_TRAIN_LABELS = './input/nouns_train_labels.txt'
+NOUNS_TEST_DOCUMENTS = './input/nouns_test_documents.txt'
+NOUNS_TEST_LABELS = './input/nouns_test_labels.txt'
+
+PAD_WORD = '<PAD>'
+MAX_SEQUENCE_LENGTH = 100
--- data_helpers.py
+++ data_helpers.py
@@ -64,4 +64,29 @@ def parse_text_data():
     print(train_df.columns)  # id, document, label
 
     parse(train_df, 'train')
-    parse(test_df, 'train')
+    parse(test_df, 'test')
+
+
+def get_docs_n_labels_from_file(doc_path, label_path):
+    """Return documents and labels from the path."""
+    docs = []
+    labels = []
+    with open(doc_path, 'r') as f:
+        for doc in f:
+            docs.append(doc.strip().split(','))
+    with open(label_path, 'r') as f:
+        for label in f:
+            labels.append(int(label.strip()))
+
+    return docs, labels
+
+
+def get_padded_seq(doc):
+    if len(doc) < consts.MAX_SEQUENCE_LENGTH:
+        doc.extend(consts.MAX_SEQUENCE_LENGTH - len(doc)) * [consts.PAD_WORD]
+    else:
+        doc = doc[:consts.MAX_SEQUENCE_LENGTH]
+    return doc
+
+
+
